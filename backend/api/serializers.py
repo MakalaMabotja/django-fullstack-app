a27de7fa083e8df5ed0d3a3fc9863fa9c25af135
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-03-31 12:20:20 +0200

    Initial migration

---
+++ backend/api/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','password']
+        extra_kwargs = {"password":{"write_only":True}}
+        
+    def create(self, validated_data):
+        """
+        This is to create a new user
+
+        Args:
+            validated_data (_type_): _description_
+
+        Returns:
+            Class: new instance of a user within the ORM
+        """
+        user = User.objects.create_user(**validated_data)
+        return user
